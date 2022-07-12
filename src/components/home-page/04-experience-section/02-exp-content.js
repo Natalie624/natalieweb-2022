69858ec95e6241ea4bed8147a1843ab53f2a8522
Natalie Cervantes <natalie.cervantes@gmail.com>
2022-07-11 17:01:01 -0700

    updated content entries for experience section

--- src/components/home-page/04-experience-section/02-exp-content.js
+++ src/components/home-page/04-experience-section/02-exp-content.js
@@ -1,22 +1,37 @@
 import * as React from 'react';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-
+import { StaticQuery, graphql } from "gatsby";
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 import * as ExpStyle from './experience.module.css';
 
 const ExperienceContent = () => (
-  <div>
+  <StaticQuery query={graphql`
+      query MyExperience{
+        contentfulMainScreenAssembly{
+          myExperience{
+            headline
+            description{
+              raw
+            }
+          }
+        }
+      }
+    `}
+  render = {data => (
+    <div>
     <AnimationOnScroll
       animateIn='animate__fadeIn'
       animateOut='animate__fadeOut'
       offset={100}
     >
-      <h3 className={`section-title text-align-center mb-2rem`}>Experience</h3>
+      <h3 className={`section-title text-align-center mb-2rem`}>{data.contentfulMainScreenAssembly.myExperience.headline}</h3>
       <p className={`text-align-center ${ExpStyle.expDescription}`}>
-        I’ve worked with and led rock-star agile teams to produce next-level
-        applications and experiences for clients such as:
+        {renderRichText(data.contentfulMainScreenAssembly.myExperience.description)}
       </p>
     </AnimationOnScroll>
   </div>
+  )}
+  />   
 );
 
 export default ExperienceContent;
